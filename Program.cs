f736614091b4524fef44b910712f7baecf3d785d
Jakub Bražina <xbrazi00@vutbr.cz>
2019-12-15 11:22:36 +0100

    Variable nazev were added

--- Program.cs
+++ Program.cs
@@ -10,6 +10,7 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
+            string nazev = "nazev";
             string Master = "Fock Off";
             Console.WriteLine("Hello World! " + Master);
             Console.ReadKey();
