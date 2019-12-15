7347000c713b48aa496930725eeffaa1bee571a9
Jakub Bražina <xbrazi00@vutbr.cz>
2019-12-15 11:32:12 +0100

    Class kocka micka were added

---
+++ Kocka.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Kocka
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Age { get; set; }
+        
+
+        //Constructor
+        public Kocka(string _name, int _age)
+        {
+            Name = _name;
+            Age = _age;            
+        }
+
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,9 +10,15 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            string Master = "Fock Off";
-            Console.WriteLine("Hello World! " + Master);
+            //string Master = "Fock Off";
+            //Console.WriteLine("Hello World! " + Master);
+            Kocka k1 = new Kocka("Micka", 5);
+
+            Console.WriteLine("Our cat named {0} is {1} years old!", k1.Name, k1.Age);
+
+
             Console.ReadKey();
+
         }
     }
 }
